a3baaf1047c4c1e41aec6a3c095827df89790129
TCSMFrogdance <58550812+TCSMFrogdance@users.noreply.github.com>
2021-09-19 00:32:41 -0700

    7

--- chart/views.py
+++ chart/views.py
@@ -7,7 +7,7 @@ def Index(request):
     return render(request, 'index.html')
 
 def GetData(request):
-    url = 'https://drive.google.com/uc?id=1PGHqWe6t28ixsPOk7KLLY9ht4XqylDW6'
+    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS9hkCVqhrG2ihyl7FIC9RJj6B_0i9_ybM3wfCINiu8iZxyBhzPP_5zP0N6nJEPuVrZuN1nFWae7wRz/pub?output=csv'
     df = pd.read_csv(url)
     data = df.to_csv()
     return HttpResponse(data)
